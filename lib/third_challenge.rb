11b2ca8886d2e3325067e5d26c5118cd4bc9d70f
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 09:17:04 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,15 +14,10 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => [{
-        name: "Benvolio",
-        age: "17",
-        attitude: "worried"
-      },{
-        name: "Mercutio",
-        age: "18",
-        attitude: "hot-headed"
-      }]
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    }, 
    :capulet => {
       :patriarch => {
@@ -38,15 +33,10 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => [{
-        name: "Steven",
-        age: "30",
-        attitude: "confused"
-      },{
-        name: "Nurse",
-        age: "44",
-        attitude: "worried" 
-      }]
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
    }
   }
 
