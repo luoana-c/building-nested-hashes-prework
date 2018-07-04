1db21282e4b6c51d542341b4d00caff8078879e3
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 09:11:32 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,13 +18,17 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague",
+        age: "54"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
