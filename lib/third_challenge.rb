52e242850ed7ea22bad833554a6d7c517b6cc503
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 09:12:10 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,11 +22,11 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        name: "Lady Montague",
-        age: "54"
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
-        name: "Romeo",
+        name: "Juliet",
         age: "15",
         status: "alive"
         },
